00e6ae0315cef5001c86c6f99bd3cad44bc18545
scem <siwanscem@gmail.com>
2024-09-21 13:15:18 +0530

    update grad

--- printlib/print.js
+++ printlib/print.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
 		html2canvas(document.querySelector('#card')).then((canvas) => {
 			let base64image = canvas.toDataURL('image/svg');
 			// console.log(base64image);
-			let pdf = new jsPDF('p', 'px', [3300, 4800]);
-			pdf.addImage(base64image, 'px', 100, 100, 637, 1015);
+			let pdf = new jsPDF('p', 'px', [2480, 3508]);
+			pdf.addImage(base64image, 'px', 284, 231, 1911, 3045);
 			pdf.save('Youridcard.pdf');
 		});
 	});
